3ece1e8d2a651599d2a7ba0484613d2b97af891c
Brett Alcox <brettalcox@gmail.com>
2017-06-06 22:51:32 -0400

    switching rest end point

--- app/controllers/temperature_controller_controller.rb
+++ app/controllers/temperature_controller_controller.rb
@@ -1,5 +1,5 @@
 class TemperatureControllerController < ApplicationController
   def retrieve_data
-    @temperature_data = HTTParty.get('http://localhost:8080/TemperatureEE-web/temperature/intervals/data.json')
+    @temperature_data = HTTParty.get('http://localhost:8080/TemperatureEE-web/temperature/intervals/day/data.json')
   end
 end
